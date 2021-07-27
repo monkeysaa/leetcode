ed0e63611aa59040ade7bddc78ebec67e9478375
Alison Cordeiro <acordeiro@gmail.com>
2021-07-27 11:58:56 -0700

    Refactor Jump_Game

--- jump_game2_BFS.py
+++ jump_game2_BFS.py
@@ -11,14 +11,23 @@ def jump_game_ii(nums):
     Your goal is to reach the last index in the minimum number of jumps.
     You can assume that you can always reach the last index.
     
-    The solution treats jumps as a tiered tree without explicitly building one. 
+    The solution treats jumps as a tiered tree without explicitly building one.
+    Each tier includes a set of indices that can be reached in that #jumps.
+    These indices can be thought of as bands. While low numbers are accessible  
+    at higher tiers, these paths are never the most efficient, and can be 
+    ignored. Thus relevant jump-index relationships might include, for example: 
+
+    jumps/tier               0     1       2       3 
+    (relevant)indices        0     1 - 2   3 - 6   7 - 14
 
     Variables: 
     - final_idx (int): Last index in nums and stopping point for the loop
-    - visited (set): A set of indices already visited, to avoid repetition.
-    - tier (list): Nums indices accessible at this "tier" 
-    - next_tier (list): Same
-    - jumps (int) - The number of jumps is akin to which tier on the tree.
+    - tier_limits (list): Lower and upper bounds of indices accessible at this 
+                    "tier". Lower bound (exclusive), Upper bound (inclusive)
+                    Doesn't include indices already accessible at lower tiers.
+    - next_jump_limit (int): Future upper bound of next tier. Initialized as 
+                    equal to upper tier limit.
+    - jumps (int) - Akin to which tier on the tree, with root at 0 jumps.
 
     >>> jump_game_ii([2,3,1,1,4])
     2
@@ -30,39 +39,30 @@ def jump_game_ii(nums):
     3
     """
 
-    final_idx = len(nums) - 1 
     jumps = 0
 
-    if final_idx == 0:
+    if len(nums) == 1:
         return jumps
 
-    tier = [0]
-    next_tier = []
-    visited = {0}
+    final_idx = len(nums) - 1 
+    tier = [-1, 0]
+    next_jump_limit = tier[1]  
 
     # Loop til it returns
     while True:
+        # Checks farthest index first in case final_idx, then closer values.
+        # Reminder: lower bound of tier tuple is exclusive. 
+        for idx in range(tier[1], tier[0], -1): # (2, 0)
+            potential_hop = nums[idx] + idx
+            if potential_hop >= final_idx:
+                return jumps + 1
 
-        for idx in tier:
-            # Pop from end of stack (to check first if goal reached)
-            node_value = nums[idx] # node_value = 2
+            if potential_hop > next_jump_limit:
+                next_jump_limit = potential_hop
 
-            for i in range(node_value, 0, -1):
-                hop_idx = idx + i
-                if hop_idx >= final_idx:
-                    return jumps + 1
-                elif hop_idx in visited:
-                    pass
-                else:
-                    visited.add(hop_idx)
-                    next_tier.append(hop_idx)
-
-        # move on to next set
+        # move on to next tier
         jumps += 1
-        tier = next_tier
-        next_tier = []
-
-
+        tier = (tier[1], next_jump_limit)
 
 
 if __name__ == "__main__":
